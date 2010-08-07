6187b8aa40e918d148b4584db6b2a82f0da90cad
Fabian Alenius <fabian.alenius@gmail.com>
2010-08-07 15:26:04 -0700

    Make the supervisor start the ewd_server

--- src/ewd_sup.erl
+++ src/ewd_sup.erl
@@ -9,8 +9,6 @@
 %% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
@@ -24,5 +22,11 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    {ok, {{one_for_one, 5, 10}, [server_spec()]}}.
+
+child(I, Type) ->
+    {I, {I, start_link, []}, permanent, 5000, Type, [I]}.
+
+server_spec() ->
+    child(ewd_server, worker).
 
