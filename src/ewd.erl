60ff269d378aa75654580de13454899c6240277a
Fabian Alenius <fabian.alenius@gmail.com>
2010-08-07 23:16:01 -0700

    Set the cookie. Add restart function. Add new function to create a new Webdriver instance.

--- src/ewd.erl
+++ src/ewd.erl
@@ -1,15 +1,23 @@
 -module(ewd).
 
--export([start/0, stop/0]).
+-export([start/0, stop/0, restart/0, new/0, new/1]).
 
 start() ->
+    erlang:set_cookie(node(), 'Yjb5XSNf'),
     application:start(ewd).
 
 stop() ->
     application:stop(ewd).
 
+restart() ->
+    application:stop(ewd),
+    application:start(ewd).
+
 new() ->
-    supRef.
+    new(firefox).
+
+new(Type) ->
+    ewd_server:new(Type).
 
 stop(Pid) ->
     ok.
